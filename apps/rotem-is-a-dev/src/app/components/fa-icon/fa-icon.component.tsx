5f6f30e5e65a2880a64d21193be78a21dc791827
Rotem Horovitz <lurxie@gmail.com>
2026-02-20 03:41:51 +0200

    refactor: promote FaIcon from cv to shared app/components
    Moves the FaIcon component to src/app/components so it can be used
    across both the cv and main sections of the app without coupling.

---
+++ apps/rotem-is-a-dev/src/app/components/fa-icon/fa-icon.component.tsx
@@ -0,0 +1,33 @@
+import type { IconGroupName } from '@/app/cv/types';
+import {
+  byPrefixAndName,
+  type IconDefinition,
+} from '@awesome.me/kit-1d40de302b/icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { isNil } from 'es-toolkit';
+
+export interface FaIconProps {
+	iconName: string;
+	iconGroup?: IconGroupName;
+	className?: string;
+}
+
+export const FaIcon = ({
+	iconName,
+	iconGroup = 'fas',
+	className,
+}: FaIconProps) => {
+	const icon: IconDefinition | undefined = byPrefixAndName[iconGroup][iconName];
+
+	if (isNil(icon)) {
+		return null;
+	}
+
+	return (
+		<FontAwesomeIcon
+      size='lg'
+			icon={icon}
+			className={className}
+		/>
+	);
+};
