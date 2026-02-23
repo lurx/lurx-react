118c93668b8c7fa5591f7458f0de68ce0cbf75f0
Rotem Horovitz <lurxie@gmail.com>
2026-02-23 02:35:28 +0200

    ✨ feat: update shiki dependency and add type declarations for shiki modules

--- apps/rotem-is-a-dev/src/app/cv/layout.tsx
+++ apps/rotem-is-a-dev/src/app/cv/layout.tsx
@@ -3,7 +3,7 @@ import './styles/global.scss';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { DownloadPdfButton } from './components/download-pdf-button';
-import type { PropsWithChildren } from 'react';
+import { Suspense, type PropsWithChildren } from 'react';
 config.autoAddCss = false;
 
 export const metadata = {
@@ -15,7 +15,9 @@ export default function CvLayout({
 }: PropsWithChildren) {
 	return (
 		<>
-			<DownloadPdfButton />
+			<Suspense>
+				<DownloadPdfButton />
+			</Suspense>
 			{children}
 		</>
 	);
