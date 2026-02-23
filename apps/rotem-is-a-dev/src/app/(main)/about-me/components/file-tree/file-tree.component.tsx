186c453e1944b4fb8b56decf91d1a57eb9873d20
Rotem Horovitz <lurxie@gmail.com>
2026-02-23 22:21:06 +0200

    cleanup

--- apps/rotem-is-a-dev/src/app/(main)/about-me/components/file-tree/file-tree.component.tsx
+++ apps/rotem-is-a-dev/src/app/(main)/about-me/components/file-tree/file-tree.component.tsx
@@ -57,61 +57,6 @@ export const FileTree = ({ activeFileId, onFileSelect }: FileTreeProps) => {
 					/>
 				);
 			})}
-
-			{/* contacts section */}
-			{/* <div className={styles.section}>
-				<button
-					type="button"
-					className={styles.folderRow}
-					aria-expanded={!collapsedSections.has('contacts')}
-					onClick={() => toggleSection('contacts')}
-				>
-					<span className={`${styles.folderChevron} ${collapsedSections.has('contacts') ? styles.collapsed : ''}`}>
-						<FaIcon
-							iconName="chevron-down"
-							iconGroup="fas"
-						/>
-					</span>
-					<span className={styles.folderIcon}>
-						<FaIcon
-							iconName={
-								collapsedSections.has('contacts')
-									? 'folder-plus'
-									: 'folder-minus'
-							}
-							iconGroup="fal"
-						/>
-					</span>
-					<span className={styles.folderLabel}>contacts</span>
-				</button>
-
-				{!collapsedSections.has('contacts') && (
-					<div className={styles.nestedItems}>
-						<div className={styles.contactRow}>
-							<span className={styles.contactIcon}>
-								<FaIcon
-									iconName="envelope"
-									iconGroup="fal"
-								/>
-							</span>
-							<span className={styles.contactLabel}>
-								lurxie@gmail.com
-							</span>
-						</div>
-						<div className={styles.contactRow}>
-							<span className={styles.contactIcon}>
-								<FaIcon
-									iconName="phone"
-									iconGroup="fal"
-								/>
-							</span>
-							<span className={styles.contactLabel}>
-								(+972) 052 522 9225
-							</span>
-						</div>
-					</div>
-				)}
-			</div> */}
 		</nav>
 	);
 };
