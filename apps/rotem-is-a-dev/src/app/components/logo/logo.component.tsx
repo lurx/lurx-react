b0338a8a2f4ee90b5a412f54935c39ae3c52b19a
Rotem Horovitz <lurxie@gmail.com>
2026-02-26 00:45:42 +0200

    ✨ feat(logo): add ICON size to logo component and update related styles

--- apps/rotem-is-a-dev/src/app/components/logo/logo.component.tsx
+++ apps/rotem-is-a-dev/src/app/components/logo/logo.component.tsx
@@ -4,13 +4,14 @@ import './logo.scss';
 import { ReactComponent as LogoSvg } from './logo.svg';
 import type { LogoProps } from './logo.types';
 
-export const Logo = ({ size, animated }: LogoProps) => {
+export const Logo = ({ size, animated, ...dataAttributes }: WithDataAttributes<LogoProps>) => {
 	const logoSize = LOGO_SIZE_OBJECTS[size ?? DEFAULT_LOGO_SIZE];
 
 	return (
 		<LogoSvg
 			className={classNames('logo-svg', { animated })}
 			{...logoSize}
+			{...dataAttributes}
 		/>
 	);
 };
--- apps/rotem-is-a-dev/src/app/components/resizable-drawer/resizable-drawer.component.tsx
+++ apps/rotem-is-a-dev/src/app/components/resizable-drawer/resizable-drawer.component.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { FaIcon } from '@/app/components/fa-icon/fa-icon.component';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
