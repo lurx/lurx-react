ed053d73676c5993a886b31f5acc98c4b8972fee
Rotem Horovitz <lurxie@gmail.com>
2026-02-27 23:23:28 +0200

    🔧 fix: hide contact me

--- apps/rotem-is-a-dev/src/app/layout/navbar/navbar.component.tsx
+++ apps/rotem-is-a-dev/src/app/layout/navbar/navbar.component.tsx
@@ -1,11 +1,11 @@
 import { toCodeLike } from '@/app/utils/to-code-like.util';
 
+import { ContactButton } from './components/contact-button.component';
+import { DownloadCVButton } from './components/download-cv-button.component';
 import { MobileMenu } from './components/mobile-menu/mobile-menu.component';
 import { NavItemsList } from './components/nav-items-list.component';
 import { NavbarLogo } from './components/navbar-logo.component';
 import styles from './navbar.module.scss';
-import { DownloadCVButton } from './components/download-cv-button.component';
-import { ContactButton } from './components/contact-button.component';
 
 export const Navbar = () => {
 	const title = toCodeLike('Rotem Horovitz', { convertCase: 'kebab-case' });
@@ -18,7 +18,7 @@ export const Navbar = () => {
 				<NavbarLogo title={title} />
 				<NavItemsList />
 				<DownloadCVButton />
-				<ContactButton />
+				<ContactButton hidden/>
 				<MobileMenu />
 			</nav>
 		</header>
